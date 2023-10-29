202d4722f0788227357fd4e09514a69cbdc86b6b
YashThussu <86000376+YashThussu@users.noreply.github.com>
2023-10-30 01:12:42 +0530

    Update rg_data_cleaning2.py

--- rg_data_cleaning2.py
+++ rg_data_cleaning2.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import numpy as np
 
-pd.set_option('display.max_columns', 500)
+pd.set_option('display.max_columns', 400)
 pd.set_option('display.width', 300)
+
+
 
 
 class DataCleaner:
